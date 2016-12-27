464a2ecdb7ff0a0b8eb6717efc018c69acdcd78d
Kenneth Galle <ken.galle@rainshowers.org>
2016-12-27 15:56:36 -0500

    port go Go done; almost working

--- gae/gaeInterface.go
+++ gae/gaeInterface.go
@@ -5,29 +5,108 @@ import (
 	"html"
 	"log"
 	"net/http"
-	"time"
+
+	"github.com/gotk3/gotk3/gtk"
+	"github.com/sourcegraph/go-webkit2/webkit2"
 )
+
+type Authenticate struct {
+	*gtk.Window // default member
+	callback    AuthenticateComplete
+	found       bool
+	code        string
+	cancelled   bool
+}
+
+type AuthenticateComplete func(auth *Authenticate) bool
+
+func NewAuthenticate(parentWindow *gtk.Window, authComplete AuthenticateComplete) *Authenticate {
+	auth := new(Authenticate)
+	auth.Window = parentWindow
+	auth.callback = authComplete
+
+	// create new signal
+	//http://zetcode.com/gui/pygtk/signals/
+	//	GObject.type_register(GaeAuthenicate)
+	//	GObject.signal_new("authenticate_complete", GaeAuthenicate, GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, ())
+
+	return auth
+}
+
+func (auth *Authenticate) GetCode() string {
+	return auth.code
+}
+
+func (auth *Authenticate) GetFound() bool {
+	return auth.found
+}
+
+func (auth *Authenticate) GetCancelled() bool {
+	return auth.cancelled
+}
 
 //	"cmd/internal/pprof/tempfile"
 
-// returns the code if successful, or non-nil error if not.
-// returns (code, err)
-func Authenicate(scope string, clientId string) (string, error) {
+// Authenicate returns the code if successful, or non-nil error if not (code, err).
+func (auth *Authenticate) Run(scope string, clientID string) (string, error) {
+	// create window for browser
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create authenticate window:", err)
+	}
+	win.SetTitle("Authenticate")
+	win.SetModal(true)
+	win.SetTransientFor(auth.Window)
+	win.SetDestroyWithParent(true)
+	win.Connect("destroy", func() bool {
+		if (!auth.found) && (!auth.cancelled) {
+			auth.cancelled = true
+		}
+		auth.callback(auth)
+		fmt.Printf("B1")
+		return false // let the window close
+	})
+
+	vbox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 6)
+	if err != nil {
+		log.Fatal("Unable to create vertical box:", err)
+	}
+	win.Add(vbox)
+
+	webView := webkit2.NewWebView()
+	vbox.Add(webView)
+
+	cancelButton, err := gtk.ButtonNewWithLabel("Cancel")
+	if err != nil {
+		log.Fatal("Unable to create cancel button:", err)
+	}
+	cancelButton.Connect("clicked", func() {
+		auth.cancelled = true
+		auth.found = false // insure consistency
+		win.Destroy()      // which will trigger win destroy event
+	})
+	vbox.Add(cancelButton)
+	win.ShowAll()
+
 	// form string
 	var code string
-	authUrl := fmt.Sprintf("https://accounts.google.com/o/oauth2/v2/auth?"+
-		"scope=%s&"+
-		"redirect_uri=http://127.0.0.1:8146&"+
-		"response_type=code&"+
-		"client_id=%s", scope, clientId)
+	authURL := "http://www.google.com"
+
+	// authURL := fmt.Sprintf("https://accounts.google.com/o/oauth2/v2/auth?"+
+	// 	"scope=%s&"+
+	// 	"redirect_uri=http://127.0.0.1:8146&"+
+	// 	"response_type=code&"+
+	// 	"client_id=%s", scope, clientID)
+
 	// start the server to listen for the results
 	// http://stackoverflow.com/a/6329459
-	fmt.Printf("B")
+	fmt.Printf("B2")
 	c := make(chan bool)
 	go func() {
 		http.HandleFunc("/",
 			func(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+				// http://stackoverflow.com/a/25606975
 				tempcode := r.URL.Query().Get("code")
 				if len(tempcode) != 0 {
 					code = tempcode
@@ -41,14 +120,8 @@ func Authenicate(scope string, clientId string) (string, error) {
 
 	// do some other stuff here while the blocking function runs
 	// make the call
-	// http://stackoverflow.com/a/25344458
-	timeout := time.Duration(15 * time.Second)
-	client := http.Client{
-		Timeout: timeout,
-	}
-	_, err := client.Get(authUrl)
+	webView.LoadURI(authURL)
 	fmt.Printf("D")
-	// webView.load_uri(auth_url)
 
 	// wait for the blocking function to finish if it hasn't already
 	<-c
--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
-import "fmt"
-import "github.com/kagalle/signetie/client_golang/gae"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
+	"github.com/kagalle/signetie/client_golang/gae"
+)
 
 func main() {
-	fmt.Printf("A")
-	gae.Authenicate("https://www.googleapis.com/auth/userinfo.profile",
-		"192820621204-nrkum19gt8a7hjrrkrdpdhh2qgmi0toq.apps.googleusercontent.com")
+	fmt.Printf("A1")
+	// Initialize GTK without parsing any command line arguments.
+	gtk.Init(nil)
+	fmt.Printf("A2")
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	fmt.Printf("A3")
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.SetTitle("Signetie")
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+	fmt.Printf("A4")
+
+	vbox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 6)
+	if err != nil {
+		log.Fatal("Unable to create vertical box:", err)
+	}
+	win.Add(vbox)
+	fmt.Printf("A5")
+
+	auth := gae.NewAuthenticate(win, func(auth *gae.Authenticate) bool {
+		continueOn := false
+		if auth.GetFound() {
+			fmt.Printf("Code obtained %s", auth.GetCode())
+			continueOn = true
+		} else if auth.GetCancelled() {
+			fmt.Printf("User canceled authenticate")
+		} else {
+			fmt.Printf("Unexpected authenticate condition")
+		}
+		return continueOn
+	})
+	fmt.Printf("A6")
+
+	runButton, err := gtk.ButtonNewWithLabel("Run")
+	if err != nil {
+		log.Fatal("Unable to create run button:", err)
+	}
+	runButton.Connect("clicked", func() {
+		fmt.Printf("M1")
+		glib.IdleAdd(auth.Run,
+			"https://www.googleapis.com/auth/userinfo.profile",
+			"192820621204-nrkum19gt8a7hjrrkrdpdhh2qgmi0toq.apps.googleusercontent.com")
+		fmt.Printf("M2")
+	})
+	vbox.Add(runButton)
+	fmt.Printf("A7")
+
+	win.ShowAll()
+	gtk.Main()
+
 	fmt.Printf("Z")
 }
