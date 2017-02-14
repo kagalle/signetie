5e44fc3069681a8710b6a7725893dbc94cc360da
Kenneth Galle <ken.galle@rainshowers.org>
2017-02-13 22:02:41 -0500

    debugging RequestAccessToken

--- gae/gaeAccessToken/codeToken.go
+++ gae/gaeAccessToken/codeToken.go
@@ -1,14 +1,8 @@
 package gaeAccessToken
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"net/url"
-	"time"
-
-	"github.com/go-errors/errors"
-	"github.com/kagalle/signetie/client_golang/gae/util"
 )
 
 // RequestAccessToken takes code obtained from previous step and converts it into a token.
@@ -16,40 +10,52 @@ func RequestAccessToken(authCode string, clientID string, clientSecret string,
 	redirectURI string) (*TokenSet, error) {
 
 	params := url.Values{}
-	state := util.RandomDataBase64url(32)
+	// state := util.RandomDataBase64url(32)
 	params.Set("code", authCode)
 	params.Set("client_id", clientID)
 	params.Set("client_secret", clientSecret)
 	// The redirect_uri comes from the "Download JSON" button in the edit client_id screen in the API Manager.
 	// Apparently for type "other" it can't be edited - you are just assigned this as a usable value.
 	params.Set("redirect_uri", redirectURI)
-
+	fmt.Printf("redirect_uri: %s\n", redirectURI)
 	params.Set("grant_type", "authorization_code")
-	params.Set("state", state)
-	resp, err := http.PostForm("https://www.googleapis.com/oauth2/v4/token", params)
-	if err != nil {
-		return nil, errors.WrapPrefix(err, "Unable to convert code into token", 0)
-	}
-	defer resp.Body.Close()
-	jsonResponse := new(responseData)
-	err = json.NewDecoder(resp.Body).Decode(jsonResponse)
-	if err != nil {
-		return nil, errors.WrapPrefix(err, "Unable to parse token response", 0)
-	}
-	tokenSet := new(TokenSet)
-	tokenSet.AccessToken = jsonResponse.AccessToken
-	tokenSet.IDToken = jsonResponse.IDToken
-	tokenSet.RefreshToken = jsonResponse.RefreshToken
-	tokenSet.ExpiresOn = time.Now() // this is a safe starting value
-	expIn := jsonResponse.ExpiresIn
-	if expIn > 0 {
-		expInStr := fmt.Sprintf("%ds", expIn)
-		duration, err := time.ParseDuration(expInStr)
-		if err == nil {
-			tokenSet.ExpiresOn.Add(duration)
-		}
-	}
-	return tokenSet, nil
+	// params.Set("state", state)
+	/*
+			resp, err := http.PostForm("https://www.googleapis.com/oauth2/v4/token", params)
+			if err != nil {
+				return nil, errors.WrapPrefix(err, "Unable to convert code into token", 0)
+			}
+			var buf bytes.Buffer
+			tee := io.TeeReader(resp.Body, &buf)
+			b, err := ioutil.ReadAll(&buf)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%s", b)
+			defer resp.Body.Close()
+			decoder := json.NewDecoder(tee)
+			jsonResponse := new(responseData)
+			err = decoder.Decode(jsonResponse)
+			if err != nil {
+				return nil, errors.WrapPrefix(err, "Unable to parse token response", 0)
+			}
+			fmt.Printf("response: %s\n", jsonResponse)
+			tokenSet := new(TokenSet)
+			tokenSet.AccessToken = jsonResponse.AccessToken
+			tokenSet.IDToken = jsonResponse.IDToken
+			tokenSet.RefreshToken = jsonResponse.RefreshToken
+			tokenSet.ExpiresOn = time.Now() // this is a safe starting value
+			expIn := jsonResponse.ExpiresIn
+			if expIn > 0 {
+				expInStr := fmt.Sprintf("%ds", expIn)
+				duration, err := time.ParseDuration(expInStr)
+				if err == nil {
+					tokenSet.ExpiresOn.Add(duration)
+				}
+			}
+		return tokenSet, nil
+	*/
+	return new(TokenSet), nil
 }
 
 /*
