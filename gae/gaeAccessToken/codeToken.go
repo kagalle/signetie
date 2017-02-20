c083fbcaf12167ddeab5133d1b9b95324d4be059
Kenneth Galle <ken.galle@rainshowers.org>
2017-02-19 22:40:01 -0500

    fix problem with json parse; add test used to figure out the problem

--- gae/gaeAccessToken/codeToken.go
+++ gae/gaeAccessToken/codeToken.go
@@ -1,8 +1,13 @@
 package gaeAccessToken
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/url"
+	"time"
+
+	"github.com/go-errors/errors"
 )
 
 // RequestAccessToken takes code obtained from previous step and converts it into a token.
@@ -20,42 +25,32 @@ func RequestAccessToken(authCode string, clientID string, clientSecret string,
 	fmt.Printf("redirect_uri: %s\n", redirectURI)
 	params.Set("grant_type", "authorization_code")
 	// params.Set("state", state)
-	/*
-			resp, err := http.PostForm("https://www.googleapis.com/oauth2/v4/token", params)
-			if err != nil {
-				return nil, errors.WrapPrefix(err, "Unable to convert code into token", 0)
-			}
-			var buf bytes.Buffer
-			tee := io.TeeReader(resp.Body, &buf)
-			b, err := ioutil.ReadAll(&buf)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("%s", b)
-			defer resp.Body.Close()
-			decoder := json.NewDecoder(tee)
-			jsonResponse := new(responseData)
-			err = decoder.Decode(jsonResponse)
-			if err != nil {
-				return nil, errors.WrapPrefix(err, "Unable to parse token response", 0)
-			}
-			fmt.Printf("response: %s\n", jsonResponse)
-			tokenSet := new(TokenSet)
-			tokenSet.AccessToken = jsonResponse.AccessToken
-			tokenSet.IDToken = jsonResponse.IDToken
-			tokenSet.RefreshToken = jsonResponse.RefreshToken
-			tokenSet.ExpiresOn = time.Now() // this is a safe starting value
-			expIn := jsonResponse.ExpiresIn
-			if expIn > 0 {
-				expInStr := fmt.Sprintf("%ds", expIn)
-				duration, err := time.ParseDuration(expInStr)
-				if err == nil {
-					tokenSet.ExpiresOn.Add(duration)
-				}
-			}
-		return tokenSet, nil
-	*/
-	return new(TokenSet), nil
+	resp, err := http.PostForm("https://www.googleapis.com/oauth2/v4/token", params)
+	if err != nil {
+		return nil, errors.WrapPrefix(err, "Unable to convert code into token", 0)
+	}
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	var jsonResponse ResponseData
+	err = decoder.Decode(&jsonResponse)
+	if err != nil {
+		return nil, errors.WrapPrefix(err, "Unable to parse token response", 0)
+	}
+	fmt.Printf("response: %+v\n", jsonResponse)
+	tokenSet := new(TokenSet)
+	tokenSet.AccessToken = jsonResponse.AccessToken
+	tokenSet.IDToken = jsonResponse.IDToken
+	tokenSet.RefreshToken = jsonResponse.RefreshToken
+	tokenSet.ExpiresOn = time.Now() // this is a safe starting value
+	expIn := jsonResponse.ExpiresIn
+	if expIn > 0 {
+		expInStr := fmt.Sprintf("%ds", expIn)
+		duration, err := time.ParseDuration(expInStr)
+		if err == nil {
+			tokenSet.ExpiresOn.Add(duration)
+		}
+	}
+	return tokenSet, nil
 }
 
 /*
@@ -66,11 +61,12 @@ func RequestAccessToken(authCode string, clientID string, clientSecret string,
  "refresh_token": "1/pt3Ihng4jCfKwjh176zO7WyqMuycHhbsU0YdJ4mb9MA",
  "id_token": "eyJ....."
 */
-type responseData struct {
-	Error            string `json:error`
-	ErrorDescription string `json:error_description`
-	AccessToken      string `json:access_token`
-	ExpiresIn        int    `json:expires_in`
-	RefreshToken     string `json:refresh_token`
-	IDToken          string `json:id_token`
+type ResponseData struct {
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
+	AccessToken      string `json:"access_token"`
+	TokenType        string `json:"token_type"`
+	ExpiresIn        int    `json:"expires_in"`
+	RefreshToken     string `json:"refresh_token"`
+	IDToken          string `json:"id_token"`
 }
